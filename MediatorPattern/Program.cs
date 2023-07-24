4e0b4d5b10bf7ae1fc45c07d0eae5c157ed9086f
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-07-24 12:32:49 +0530

    mediator design pattern

--- MediatorPattern/Program.cs
+++ MediatorPattern/Program.cs
@@ -9,19 +9,11 @@
             User Dave = new ConcreteUser("Dave",2);
             User Smith = new ConcreteUser("Smith",3);
             User Rajesh = new ConcreteUser("Rajesh",4);
-            User Sam = new ConcreteUser("Sam",5);
-            User Pam = new ConcreteUser("Pam",6);
-            User Anurag = new ConcreteUser("Anurag",7);
-            User John = new ConcreteUser("John",8);
 
             facebookMediator.RegisterUser(Ram);
             facebookMediator.RegisterUser(Dave);
             facebookMediator.RegisterUser(Smith);
             facebookMediator.RegisterUser(Rajesh);
-            facebookMediator.RegisterUser(Sam);
-            facebookMediator.RegisterUser(Pam);
-            facebookMediator.RegisterUser(Anurag);
-            facebookMediator.RegisterUser(John);
 
             Dave.Send("Design Pattern");
 
